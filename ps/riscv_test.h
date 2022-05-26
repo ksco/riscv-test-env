784ad2184e410a75da0c47528715feb28568192c
ksco <numbksco@gmail.com>
2022-05-27 00:19:58 +0800

    Add missing macros in ps env

--- ps/riscv_test.h
+++ ps/riscv_test.h
@@ -19,6 +19,11 @@
   RVTEST_FP_ENABLE;                                                     \
   .endm
 
+#define RVTEST_RV64UV                                                   \
+  .macro init;                                                          \
+  RVTEST_VECTOR_ENABLE;                                                 \
+  .endm
+
 #define RVTEST_RV32U                                                    \
   .macro init;                                                          \
   .endm
@@ -26,6 +31,11 @@
 #define RVTEST_RV32UF                                                   \
   .macro init;                                                          \
   RVTEST_FP_ENABLE;                                                     \
+  .endm
+
+#define RVTEST_RV32UV                                                   \
+  .macro init;                                                          \
+  RVTEST_VECTOR_ENABLE;                                                 \
   .endm
 
 #define RVTEST_RV64M                                                    \
@@ -94,6 +104,13 @@
   li a0, MSTATUS_FS & (MSTATUS_FS >> 1);                                \
   csrs mstatus, a0;                                                     \
   csrwi fcsr, 0
+
+#define RVTEST_VECTOR_ENABLE                                            \
+  li a0, (MSTATUS_VS & (MSTATUS_VS >> 1)) |                             \
+         (MSTATUS_FS & (MSTATUS_FS >> 1));                              \
+  csrs mstatus, a0;                                                     \
+  csrwi fcsr, 0;                                                        \
+  csrwi vcsr, 0;
 
 #define RISCV_MULTICORE_DISABLE                                         \
   csrr a0, mhartid;                                                     \
